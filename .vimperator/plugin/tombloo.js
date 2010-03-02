e400c9d13fb03b0d90f748484dc890719c9da4fd
mashiro <y.mashiro@gmail.com>
2010-03-02 21:53:25 +0900

    rev4230 にバージョンを上げた feedSomeKeys_2.js から feedSomeKeys_3.js に移行

--- .vimperator/plugin/tombloo.js
+++ .vimperator/plugin/tombloo.js
@@ -4,9 +4,9 @@ let PLUGIN_INFO =
 <description>Tombloo integrate plugin</description>
 <description lang="ja">Tombloo 統合プラグイン</description>
 <author>Trapezoid</author>
-<version>0.1e</version>
+<version>0.1.1</version>
 <minVersion>2.0pre</minVersion>
-<maxVersion>2.0pre</maxVersion>
+<maxVersion>2.3</maxVersion>
 <updateURL>http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk/tombloo.js</updateURL>
 <detail><![CDATA[
 
@@ -52,13 +52,14 @@ with (tomblooService) {
 commands.addUserCommand(
     ['tomblooAction'],
     'Execute Tombloo actions',
-    function (arg) {
-        let f = Tombloo.Service.actions[arg.string];
+    function (args) {
+        let f = Tombloo.Service.actions[args.literalArg];
         (f instanceof Function)
             ? f.execute()
-            : liberator.echoerr(arg.string + ' is not Tombloo Action.');
+            : liberator.echoerr(args.literalArg + ' is not Tombloo Action.');
     },
     {
+        literal: 0,
         completer: function (context) {
             context.title = ['Tombloo Actions'];
 
@@ -76,17 +77,14 @@ commands.addUserCommand(
     ['tombloo'],
     'Post by Tombloo',
     function (args) {
-        //let f = Tombloo.Service.extractors[args.string];
-        let arg = args.string.replace(/\\(?=\u0020)/g, '');
-        liberator.log(args.string, 0);
-        liberator.log(arg, 0);
-
-        let f = Tombloo.Service.extractors[arg];
+        liberator.log(args.literalArg, 0);
+        let f = Tombloo.Service.extractors[args.literalArg];
         (typeof f === 'object')
             ? Tombloo.Service.share(getContext(), f, args.bang)
             : liberator.echoerr(args.string + ' is not Tombloo command');
     },
     {
+        literal: 0,
         bang: true,
         completer: function (context) {
             context.title = ['Tombloo'];
