1aa6135caff0c3b93da74e3f08083710419c53b6
mashiro <mail@mashiro.org>
2016-06-14 14:51:14 +0900

    follow head

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -3,11 +3,6 @@
 brew update
 brew upgrade
 
-brew tap homebrew/services
-brew tap caskroom/homebrew-cask
-brew tap caskroom/homebrew-versions
-brew install brew-cask
-
 brew install zsh
 brew install tmux
 brew install emacs
@@ -22,7 +17,7 @@ brew install readline
 
 brew install lua luajit
 brew install python3
-brew install macvim --HEAD --override-system-vim --with-lua --with-luajit --with-python3
+brew install vim --with-override-system-vim --with-lua --with-luajit --with-python3
 
 brew install rbenv
 brew install ruby-build
@@ -34,9 +29,9 @@ brew cask install seil
 brew cask install iterm2
 brew cask install atom
 brew cask install google-japanese-ime
-brew cask install firefox-ja --appdir=/Applications
-brew cask install google-chrome --appdir=/Applications
-brew cask install google-chrome-canary --appdir=/Applications
+brew cask install firefox-ja
+brew cask install google-chrome
+brew cask install google-chrome-canary
 brew cask install istat-menus
 brew cask install gyazo
 brew cask install dropbox
