620b1a63969ea102d038aec5feeb6a303919ba9b
mashiro <mail@mashiro.org>
2018-06-19 16:53:20 +0900

    Update homebrew packages

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -2,24 +2,26 @@
 brew update
 brew upgrade
 
-brew install zsh
-brew install tmux
-brew install emacs
+brew install readline
+
+brew install luajit
+brew install rbenv
+brew install ruby-build
+brew install pyenv
+brew install pipenv
+brew install python3
+brew install go
+brew install nodebrew
+
 brew install wget
 brew install tree
 brew install jq
+brew install jo
 brew install direnv
 brew install imagemagick
 brew install the_silver_searcher
 brew install reattach-to-user-namespace
-brew install readline
-
-brew install luajit
-brew install python3
+brew install tmux
+brew install zsh
 brew install vim --with-override-system-vim --with-luajit --with-python3
-
-brew install rbenv
-brew install ruby-build
-brew install pyenv
-brew install pyenv-virtualenv
 
